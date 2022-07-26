92c12c4a288f2abf704cd1d18cced80bba4aacf0
BrenoAnjos <108766558+BrenoAnjos@users.noreply.github.com>
2022-07-26 18:06:51 -0300

    Update sketch.js

--- C12-project-template-main/sketch.js
+++ C12-project-template-main/sketch.js
@@ -1,25 +1,29 @@
 var path,boy, leftBoundary,rightBoundary;
 var pathImg,boyImg;
 var i;
-
+var edges
 function preload(){
   //loadImage (carregarImagem) da pista)
+  pathImg = loadImage("path.png")
   //loadAnimation (carregarAnimação) de corrida para o menino
- 
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png")
 }
 
 function setup(){
   
   createCanvas(400,400);
- //crie um sprite para a pista 
+ //crie um sprite para a pista
+ path = createSprite(200,180,400,20)
 //adicione uma imagem para a pista
+path.addImage(pathImg)
 //Faça com que a pista seja um fundo que se move dando a ela velocity Y.
-path.scale=1.2;
+path.scale = 1.2;
 
 //crie um sprite de menino
+boy = createSprite(200,330,20,100)
 //adicione uma animação de corrida para ele
-boy.scale=0.08;
-  
+  boy.addAnimation("boy",boyImg)
+  boy.scale = 0.05
 //crie um limite à esquerda
 leftBoundary=createSprite(0,0,100,800);
 //defina visibilidade como falsa para o limite à esquerda
@@ -34,9 +38,9 @@ function draw() {
   path.velocityY = 4;
   
   // mover o menino com o mouse usando mouseX
-  
-  edges= createEdgeSprites();
-  boy.collide(edges[3]);
+  boy.x = World.mouseX
+  // edges= createEdgeSprites();
+  // boy.collides(edges[3]);
   // colidir o menino com os limites invisíveis da esquerda e da direita
   
   //código para redefinir o fundo
